70ec6884f83a58bde86d6842e056fc775b0632f0
Tobias Boogh <t@boogh.se>
2021-12-06 20:58:25 +0100

    Part 2 done, grouping the values increased the speed amazingly

--- Sources/AdventOfCode-2021/Day_6.swift
+++ Sources/AdventOfCode-2021/Day_6.swift
@@ -6,12 +6,40 @@ public class Day_6 {
         return compute(values: data, days: days)
     }
 
-    func partTwo(input: [Int]) -> Int {
-        return 0
+    func partTwo(input: String, days: Int) -> Int {
+        let data = input.components(separatedBy: ",")
+            .compactMap { Int($0) }
+        return computeCalculated(values: data, days: days)
     }
 }
 
 private extension Day_6 {
+
+    func computeCalculated(values: [Int], days: Int) -> Int {
+        var groups = Dictionary(grouping: values, by: { $0 }).mapValues { $0.count }
+        for day in 1...days {
+            var newCount = 0
+            var updated = groups.map { (key, value) -> (Int, Int) in
+                var newkey = key
+                if key == 0 {
+                    newCount += value
+                    newkey = 6
+                } else {
+                    newkey = key - 1
+                }
+                return (newkey, value)
+            }
+
+            if newCount > 0 {
+                updated.append((8, newCount))
+            }
+            let updatedGroups = Dictionary(grouping: updated) { $0.0 }
+                .mapValues { $0.map { $0.1 }.reduce(0, +) }
+            groups = updatedGroups
+        }
+
+        return groups.values.reduce(0, +)
+    }
 
     func compute(values: [Int], days: Int) -> Int {
         var fishes = values
--- Tests/AdventOfCode-2021Tests/TestDay_6.swift
+++ Tests/AdventOfCode-2021Tests/TestDay_6.swift
@@ -13,20 +13,22 @@ final class TestDay_6: XCTestCase {
     func testComputePartOne() {
         let input = loadTest(filename: "Day6").trimmingCharacters(in: .newlines)
         let day = Day_6()
-        let result = day.partOne(input: input, days: 80)
+        let result = day.partTwo(input: input, days: 80)
         XCTAssertEqual(result, 361169)
     }
 
     func testExamplePartTwo() {
-        //TODO: This can probably be calculated, the day needs to be offset to be at's 0 then it should be division problem
         let input = "3,4,3,1,2"
         let day = Day_6()
-        let result = day.partOne(input: input, days: 256)
-        XCTAssertEqual(result, 5934)
+        let result = day.partTwo(input: input, days: 4)
+        XCTAssertEqual(result, 9)
     }
 
     func testComputePartTwo() {
-
+        let input = loadTest(filename: "Day6").trimmingCharacters(in: .newlines)
+        let day = Day_6()
+        let result = day.partTwo(input: input, days: 256)
+        XCTAssertEqual(result, 1634946868992)
     }
 }
 
